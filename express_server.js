e829394ba0afd05feb61ae2832530559e5bf059c
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-20 13:22:43 -0500

    Add a route for /urls

--- express_server.js
+++ express_server.js
@@ -22,6 +22,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get('/urls', (req, res) => {
+  //⬇️When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. This is so we can use the key of that variable (in the above case the key is urls) to access the data within our template.
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
 // app.get('*', (req, res) => {
 //   res.send(`404 Page Not Found`);
 // });
