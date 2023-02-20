1257d75eb9271361c7b75661afd1d6fd73cd234e
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-20 13:08:30 -0500

    Set up EJS

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+//Setting up EJS
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -18,6 +21,10 @@ app.get(`/urls.json`, (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// app.get('*', (req, res) => {
+//   res.send(`404 Page Not Found`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
