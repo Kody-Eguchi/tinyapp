6e523bf48b1142442957b6663e5192632af18946
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-20 16:00:16 -0500

    Add a route for /urls/new

--- express_server.js
+++ express_server.js
@@ -28,11 +28,17 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render("urls_new");
+});
+
 //⬇️WThe end point for such a page will be in the format /urls/:id. The : in front of id indicates that id is a route parameter. This means that the value in this part of the url will be available in the req.params object.
 app.get('/urls/:id', (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
+
 
 // app.get('*', (req, res) => {
 //   res.send(`404 Page Not Found`);
